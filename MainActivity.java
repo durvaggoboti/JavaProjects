86d1d3f3f94e94bb6216fed77ff352fce9805bd2
durvaggoboti <uponnasweety21@gmail.com>
2016-08-22 12:59:10 +0600

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,27 @@
+package com.a4bit.we.mycontentprovider;
+
+import android.content.ContentValues;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickAddName(View view) {
+        ContentValues values = new ContentValues();
+        values.put(MyProvider.name, ((EditText) findViewById(R.id.txtName))
+                .getText().toString());
+        Uri uri = getContentResolver().insert(MyProvider.CONTENT_URI, values);
+        Toast.makeText(getBaseContext(), "New record inserted", Toast.LENGTH_LONG)
+                .show();
+    }
+}
